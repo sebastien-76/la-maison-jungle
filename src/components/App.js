4c446c7d4fb0318e9c08622cb3608418d29fa4ff
sebastien <sebastien.divers@gmail.com>
2024-05-02 16:58:33 +0200

    Ajout panier, bouton ajouter, filtre par categories

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,27 @@
-import Banner from "./Banner";
-import Cart from "./Cart";
-import ShoppingList from "./ShoppingList";
+import Banner from './Banner'
+import logo from '../assets/logo.png'
+import Cart from './Cart'
+import Footer from './Footer'
+import ShoppingList from './ShoppingList'
+import { useState } from 'react'
+import '../styles/Layout.css'
 
 function App() {
-  return (
-    <div>
-      <Banner />
-      <Cart />
-      <ShoppingList />
-    </div>
-  );
+    const [cart, updateCart] = useState([])
+    
+    return (
+        <div>
+            <Banner>
+                <img src={logo} alt='La maison jungle' className='lmj-logo' />
+                <h1 className='lmj-title'>La maison jungle</h1>
+            </Banner>
+            <div className='lmj-layout-inner'>
+                <Cart cart={cart} updateCart={updateCart} />
+                <ShoppingList cart={cart} updateCart={updateCart} />
+            </div>
+            <Footer />
+        </div>
+    )
 }
 
-export default App;
+export default App
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,27 +1,57 @@
-import { plantList } from '../datas/plantList'
+import { useState } from "react";
+import { plantList } from "../datas/plantList";
+import Categories from './Categories'
+import PlantItem from "./PlantItem";
+import "../styles/ShoppingList.css";
 
+function ShoppingList({ cart, updateCart }) {
+  const [activeCategory, setActiveCategory] = useState("");
+  const categories = plantList.reduce(
+    (acc, plant) =>
+      acc.includes(plant.category) ? acc : acc.concat(plant.category),
+    []
+  );
+  function addToCart(name, price) {
+    const currentPlantSaved = cart.find((plant) => plant.name === name);
+    if (currentPlantSaved) {
+      const cartFilteredCurrentPlant = cart.filter(
+        (plant) => plant.name !== name
+      );
+      updateCart([
+        ...cartFilteredCurrentPlant,
+        { name, price, amount: currentPlantSaved.amount + 1 },
+      ]);
+    } else {
+      updateCart([...cart, { name, price, amount: 1 }]);
+    }
+  }
 
-function ShoppingList() {
-	const categories = plantList.reduce(
-		(acc, plant) =>
-			acc.includes(plant.category) ? acc : acc.concat(plant.category),
-		[]
-	)
-
-	return (
-		<div>
-			<ul>
-				{categories.map((cat) => (
-					<li key={cat}>{cat}</li>
-				))}
-			</ul>
-			<ul>
-				{plantList.map((plant) => (
-					<li key={plant.id}>{plant.name}</li>
-				))}
-			</ul>
-		</div>
-	)
+  return (
+    <div>
+     <Categories
+				categories={categories}
+				setActiveCategory={setActiveCategory}
+				activeCategory={activeCategory}
+			/>
+      <ul className="lmj-plant-list">
+        {plantList.map(({ id, cover, name, water, light, price, category }) => 
+			!activeCategory || activeCategory === category ? (
+          <div key={id}>
+            <PlantItem
+              cover={cover}
+              name={name}
+              water={water}
+              light={light}
+              price={price}
+            />
+            <button onClick={() => addToCart(name, price)}>Ajouter</button>
+          </div>
+		  
+        ) : null
+		)}
+      </ul>
+    </div>
+  );
 }
 
-export default ShoppingList
+export default ShoppingList;
